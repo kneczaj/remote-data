6f757b7ffc81b696262e51485e5214974afec04b
Kamil Neczaj <kneczaj@gmail.com>
2017-11-07 00:29:48 +0100

    Second draft of REST service

--- src/lib/rest/rest.service.ts
+++ src/lib/rest/rest.service.ts
@@ -1,17 +1,116 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
-import {Serializable} from './serializable';
+import {isNull} from 'util';
+
+/**
+ * An abstract class for objects exchanged with a REST backend
+ */
+export abstract class RestItem {
+
+  /**
+   * To be assigned by the class factory
+   *
+   * TODO: encapsulate these three and let to set it just by the service
+   */
+  public static resourceUrl: string;
+  public static http: Http;
+  public static requestOptions: RequestOptions;
+
+  private _id: number = null;
+
+  get id() {
+    return this._id;
+  }
+
+  set id(value: number) {
+    if (isNull(this._id)) {
+      this._id = value;
+    } else {
+      throw `Error: cannot set the id for an object with id ${this._id}`;
+    }
+  }
+
+  /**
+   * Serializes object to backend data format
+   */
+  abstract dump(): {};
+
+  /**
+   * Create object from backend data.
+   *
+   * NOTE: should set the id;
+   */
+  abstract load(data: {});
+
+  /**
+   * Makes DELETE request to delete the object from the DB, as the object gets deleted the ID is set to null
+   * @returns {Observable<RestItem>}
+   */
+  public delete() {
+    const observable = RestItem.http.delete(`${RestItem.resourceUrl}/${this.id}`, RestItem.requestOptions);
+    observable.subscribe(request => {
+      this._id = null;
+    });
+    // TODO: wait for the ID to be unset
+    return observable.map(data => this);
+  }
+
+  public save(): Observable<RestItem> {
+    if (!this._id) {
+      return this.create();
+    }
+    this.update();
+  }
+
+  /**
+   * Makes POST request to save the object to the DB and assign an ID.
+   * @returns {Observable<RestItem>}
+   */
+  protected create(): Observable<RestItem> {
+    const observable = RestItem.http.post(RestItem.resourceUrl, this.dump(), RestItem.requestOptions);
+    observable.subscribe(request => {
+      this.id = request.json().id;
+    });
+    // TODO: wait for the ID to be set
+    return observable.map(data => this);
+  }
+
+  /**
+   * Makes PUT request to save the object to the DB and assign an ID.
+   * @returns {Observable<RestItem>}
+   */
+  protected update(): Observable<RestItem> {
+    return RestItem.http.put(
+      `${RestItem.resourceUrl}/${this.id}`,
+      this.dump(),
+      RestItem.requestOptions
+    ).map(data => this);
+  }
+}
 
 /**
  * A class to communicate with rest interface
  *
- * TODO: inject the service to objects to be able to save them with T.save() method of the object
  */
 @Injectable()
-export class RestService<T> {
-  constructor(private http: Http, private resourceUrl: string) {
-
+export class RestService<T extends RestItem> {
+  /**
+   * Constructor
+   * @param {Http} http
+   * @param {RequestOptions} requestOptions
+   * @param {string} resourceUrl
+   * @param {any} ResourceClass - the same class as the generic T
+   */
+  constructor(
+    private http: Http,
+    private requestOptions: RequestOptions,
+    private resourceUrl: string,
+    private ResourceClass: any
+  ) {
+    this.ResourceClass.http = this.http;
+    this.ResourceClass.resourceUrl = this.http;
+    this.ResourceClass.requestOptions = this.requestOptions;
   }
 
   getAll(): Observable<Array<T>> {
@@ -26,9 +125,8 @@ export class RestService<T> {
   }
 
   private create(data: {}): T {
-    // TODO: find proper way to create the object
-    const result = new T.constructor();
-    (result as Serializable).load(data);
+    const result = new this.ResourceClass();
+    result.load(data);
     return result;
   }
 }
